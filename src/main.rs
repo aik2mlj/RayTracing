bf409445848a6f1e7bbaae497cc24c5956dc8648
aik2mlj <897744820@qq.com>
2020-07-22 14:48:16 +0800

    色图(((

--- src/main.rs
+++ src/main.rs
@@ -9,14 +9,17 @@ fn main() {
     let x = Vec3::new(1.0, 1.0, 1.0);
     println!("{:?}", x);
 
-    let mut img: RgbImage = ImageBuffer::new(1024, 512);
-    let bar = ProgressBar::new(1024);
+    let mut img: RgbImage = ImageBuffer::new(1024, 1024);
+    let bar = ProgressBar::new(1024); // used for displaying progress in stdcerr
 
     for x in 0..1024 {
-        for y in 0..512 {
+        for y in 1..1024 {
             let pixel = img.get_pixel_mut(x, y);
-            let color = (x / 4) as u8;
-            *pixel = image::Rgb([color, color, color]);
+            // let color = (x / 4) as u8;
+            let r = (x / 4) as u8;
+            let g = (y / 4) as u8;
+            let b = (x & y) as u8;
+            *pixel = image::Rgb([r, g, b]);
         }
         bar.inc(1);
     }
